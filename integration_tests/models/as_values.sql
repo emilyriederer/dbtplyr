1fa65ce299f9b633015d63de874e94d8823a239a
Emily Riederer <emilyriederer@gmail.com>
2021-02-08 06:03:43 -0600

    update as_values tests with placeholders

--- integration_tests/models/as_values.sql
+++ integration_tests/models/as_values.sql
@@ -1,7 +1,25 @@
 select
 
-  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'n') | join(',') ~ "'"}} as starts_with_n,
-  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'ind') | join(',') ~ "'" }} as starts_with_ind,
-  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'cat') | join(',') ~ "'"}} as starts_with_cat,
+  -- starts_with
+  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'n') | join(',') ~ "'"}} as starts_with_n, -- single item
+  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'ind') | join(',') ~ "'" }} as starts_with_ind, -- multiple items
+  {{ "'" ~ dbt_dplyr.starts_with( ref('data'), 'cat') | join(',') ~ "'"}} as starts_with_cat, -- no items
+  
+  -- ends_with (TODO)
+  
+  -- matches (TODO)
+    
+  -- contains (TODO)
+  
+  -- not_contains (TODO)
+  
+  -- one_of (TODO)
+  
+  -- not_one_of (TODO)
+  
+  -- everything
+  {{ "'" ~ dbt_dplyr.everything( ref('data')) | join(',') ~ "'"}} as everything, -- no items
+  
+  1 as x -- dummy to allow trailing commas
 
   
