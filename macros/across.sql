9ef2029b6c2b105f660c33ef4da13ed7fb39779a
Emily Riederer <emilyriederer@gmail.com>
2021-02-13 11:13:19 -0600

    optionally include final comma inside

--- macros/across.sql
+++ macros/across.sql
@@ -1,8 +1,9 @@
-{% macro across(var_list, script_string) %}
+{% macro across(var_list, script_string, final_comma) %}
 
   {% for v in var_list %}
   {{ script_string | replace('{{var}}', v) }}
   {%- if not loop.last %},{% endif %}
+  {%- if loop.last and final_comma|default(false) %},{% endif %}
   {% endfor %}
 
 {% endmacro %}
