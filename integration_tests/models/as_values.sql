a0758c0b5afd95b4c963ad23cda420eeb8a75a7d
Emily Riederer <emilyriederer@gmail.com>
2021-02-14 07:27:29 -0600

    cast type conditional on target db for tests

--- integration_tests/models/as_values.sql
+++ integration_tests/models/as_values.sql
@@ -1,16 +1,23 @@
 {% set cols = dbt_dplyr.get_column_names( ref('data')) %}
+{% set cast_to %}
+  {% if target.type == 'postgres' %}
+    text
+  {% else %}
+    string
+  {% endif %}
+{% endset %}
 
 select
 
   -- starts_with
-  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'n') | join(',') ~ "'"}} as text)    as starts_with_n, -- single item
-  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'ind') | join(',') ~ "'" }} as text) as starts_with_ind, -- multiple items
-  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'cat') | join(',') ~ "'"}} as text)  as starts_with_cat, -- no items
+  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'n') | join(',') ~ "'"}} as {{cast_to}})    as starts_with_n, -- single item
+  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'ind') | join(',') ~ "'" }} as {{cast_to}}) as starts_with_ind, -- multiple items
+  cast({{ "'" ~ dbt_dplyr.starts_with(cols, 'cat') | join(',') ~ "'"}} as {{cast_to}})  as starts_with_cat, -- no items
   
   -- ends_with (TODO)
-  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'b') | join(',') ~ "'"}} as text)  as ends_with_b, -- single item
-  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'a') | join(',') ~ "'" }} as text) as ends_with_a, -- multiple items
-  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'z') | join(',') ~ "'"}} as text)  as ends_with_z, -- no items
+  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'b') | join(',') ~ "'"}} as {{cast_to}})  as ends_with_b, -- single item
+  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'a') | join(',') ~ "'" }} as {{cast_to}}) as ends_with_a, -- multiple items
+  cast({{ "'" ~ dbt_dplyr.ends_with(cols, 'z') | join(',') ~ "'"}} as {{cast_to}})  as ends_with_z, -- no items
   
   -- matches (TODO)
     
@@ -23,7 +30,7 @@ select
   -- not_one_of (TODO)
   
   -- everything
-  cast({{ "'" ~ dbt_dplyr.everything(cols) | join(',') ~ "'"}} as text) as everything, -- no items
+  cast({{ "'" ~ dbt_dplyr.everything(cols) | join(',') ~ "'"}} as {{cast_to}}) as everything, -- no items
   
   1 as x -- dummy to allow trailing commas
 
