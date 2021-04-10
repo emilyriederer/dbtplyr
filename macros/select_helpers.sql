251401ff0a9f42399c1206029dfb423d748593b2
Emily Riederer <emilyriederer@gmail.com>
2021-04-10 08:47:37 -0500

    change macros to call dbtplyr vs dbt_dplyr

--- macros/select_helpers.sql
+++ macros/select_helpers.sql
@@ -1,51 +1,51 @@
 {% macro starts_with(relation, string) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = "^" ~ string ~ ".*" %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
 
 {% macro ends_with(relation, string) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = "^.*" ~ string ~ "$" %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
 
 {% macro contains(relation, string) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = "^.*" ~ string ~ ".*$" %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
 
 {% macro not_contains(relation, string) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = "^((?!" ~ string ~ ").)*$" %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
 
 {% macro one_of(relation, strings) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = "^("+ strings|join("|") +")$" %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
 
 {% macro not_one_of(relation, strings) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set results = cols | reject('in', strings) %}
 {{return(results)}}
 
@@ -53,9 +53,9 @@
 
 {% macro matches(relation, string) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {%set regex = string %}
-{%set results = dbt_dplyr.get_matches(cols, regex) %}
+{%set results = dbtplyr.get_matches(cols, regex) %}
 {{return(results)}}
 
 {% endmacro %}
@@ -63,7 +63,7 @@
 
 {% macro everything(relation) %}
 
-{%set cols = dbt_dplyr.get_column_names(relation) %}
+{%set cols = dbtplyr.get_column_names(relation) %}
 {{return(cols)}}
 
 {% endmacro %}
