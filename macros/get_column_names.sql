41c8e107a102c9ca2ad9a9d7bf91de8c95bfa0c6
Emily Riederer <emilyriederer@gmail.com>
2021-04-10 06:06:26 -0500

    refacter gcn macro

--- macros/get_column_names.sql
+++ macros/get_column_names.sql
@@ -1,15 +1,14 @@
 {% macro get_column_names(relation) %}
 
+{# if relation is not actually a reference simply pass through #}
+{# this is useful so downstream functions can accept either list or relation #}
+
 {% if relation.database is not defined %}
-
   {{ return(relation) }}
-
-{% else %}
-
-  {%- set results_list  = adapter.get_columns_in_relation(relation) -%}
-  {{ return(results_list) }}
-
 {% endif %}
+
+{%- set results_list  = adapter.get_columns_in_relation(relation) -%}
+{{ return(results_list) }}
 
 {% endmacro %}
 
