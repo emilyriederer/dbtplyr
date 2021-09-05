2204e72dc6b8a3373869c360eb5453cd9868ac21
Emily Riederer <emilyriederer@gmail.com>
2021-09-05 10:59:58 -0500

    implement where() with if-else

--- macros/select_helpers.sql
+++ macros/select_helpers.sql
@@ -61,18 +61,30 @@
 {% endmacro %}
 
 
-{% macro where(fn, relation) %}
-
-{%set cols = adapter.get_columns_in_relation(relation) %}
-{%set results = dbtplyr.map(cols, fn) %}
-{{return(results)}}
-
-{% endmacro %}
-
-
 {% macro everything(relation) %}
 
 {%set cols = dbtplyr.get_column_names(relation) %}
 {{return(cols)}}
+
+{% endmacro %}
+
+{% macro where(fn, relation) %}
+
+  {% set cols = adapter.get_columns_in_relation(relation) %}
+  {% set results_list = [] %}
+
+  {%  for c in cols %}
+    {% if fn == "is_character" and c.is_character() %}
+      {{ results_list.append(c.name) or "" }}
+    {% elif fn == "is_number" and c.is_number() %}
+      {{ results_list.append(c.name) or "" }}
+    {% elif fn == "is_float" and c.is_float() %}
+      {{ results_list.append(c.name) or "" }}
+    {% elif fn == "is_numeric" and c.is_numeric() %}
+      {{ results_list.append(c.name) or "" }}
+    {% endif %}
+  {% endfor %}
+
+{{return(results_list)}}
 
 {% endmacro %}
